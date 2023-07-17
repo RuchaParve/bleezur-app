4fbd2478eb306e9cc911a8f50d430e223e3ae6aa
Rucha Parve <ruchaparve76@gmail.com>
2023-07-17 11:51:11 +0530

    ui changes

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -38,7 +38,58 @@ const Profiles = () => {
         <Grid sx={{margin:"20px"}}>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                   
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                First Name
+                            </TableCell>
+                            <TableCell align="right">
+                                Last Name
+                            </TableCell>
+                            <TableCell align="right">
+                                Email
+                            </TableCell>
+                            <TableCell align="right">
+                                Company Name
+                            </TableCell>
+                            <TableCell align="right">
+                                Company Url
+                            </TableCell>
+                            <TableCell align="right">
+                                Action
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {profiles.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{
+                                    '&:last-child td, &:last-child th':
+                                        { border: 0 }
+                                }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.firstName}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.lastName}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.email}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.companyName}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.companyUrl}
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Button  onClick={() => handleClick(row._id)}>View</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
                 </Table>
             </TableContainer>
         </Grid>
--- client/src/components/ProfilesData.js
+++ client/src/components/ProfilesData.js
@@ -31,44 +31,7 @@ const ProfilesData = () => {
                 <Grid item md={8}>
                     <TableContainer component={Paper}>
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableCell align="First Name"></TableCell>
-                                <TableCell align="Last Name"></TableCell>
-                                <TableCell align="Email"></TableCell>
-                                <TableCell align="Company Name"></TableCell>
-                                <TableCell align="Company Url"></TableCell>
-                                <TableCell align="Role / Designation"></TableCell>
-                                <TableCell align="Customer Profile Details"></TableCell>
-                                <TableCell align="Target Selling City"></TableCell>
-                                <TableCell align="Focused Industries"></TableCell>
-                                <TableCell align="Selling to"></TableCell>
-                                <TableCell align="Size of Company"></TableCell>
-                                <TableCell align="Revenue of Company"></TableCell>
-                                <TableCell align="Average deal size"></TableCell>
-                                <TableCell align="Good leads"></TableCell>
-                                <TableCell align="Main skills / strengths"></TableCell>
-                            </TableHead>
-                            {data.map((ele) => (
-                                <TableBody>
-                                    <TableCell component="th" scope="row">
-                                        {ele.firstName}
-                                    </TableCell>
-                                    <TableCell align="right">{ele.lastName}</TableCell>
-                                    <TableCell align="right">{ele.email}</TableCell>
-                                    <TableCell align="right">{ele.role}</TableCell>
-                                    <TableCell align="right">{ele.protein}</TableCell>
-                                    <TableCell align="right">{ele.lastName}</TableCell>
-                                    <TableCell align="right">{ele.email}</TableCell>
-                                    <TableCell align="right">{ele.role}</TableCell>
-                                    <TableCell align="right">{ele.industryName}</TableCell>
-                                    <TableCell align="right">{ele.customerProfileDetails}</TableCell>
-                                    <TableCell align="right">{ele.companyUrl}</TableCell>
-                                    <TableCell align="right">{ele.sellingProfileNames}</TableCell>
-                                    <TableCell align="right">{ele.dealSize}</TableCell>
-                                    <TableCell align="right">{ele.leads}</TableCell>
-                                    <TableCell align="right">{ele.skillsandStrengths}</TableCell>
-                                </TableBody>
-                            ))}
+                            
                         </Table>
                     </TableContainer>
                 </Grid>
